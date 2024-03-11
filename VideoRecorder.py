c573be94a48bcdd2f7f6b18085227a7f344840a4
KimDa99 <hyhy61451@gmail.com>
2024-03-11 17:11:06 +0900

    feat: controlling Contrast & Brightness with reset *ps : last commit message should have 'fliping feature'

--- VideoRecorder.py
+++ VideoRecorder.py
@@ -9,12 +9,12 @@ def Init_Video(source):
     video = cv.VideoCapture(source)
     return video
 
-def Draw_Text(img, is_recording, is_enlarged, is_fliped, is_contrast, is_brightness, is_rotate, contrast, brightness, rotate):
+def Draw_Text(img, is_recording, is_enlarged, is_fliped, is_contrast, is_brightness, contrast, brightness):
     frame_with_text = img.copy()
 
     # Draw a transparent rectangle
     overlay = frame_with_text.copy()
-    cv.rectangle(overlay, (10, 10), (250, 45 + 20*8), color=(0, 0, 0), thickness=-1)
+    cv.rectangle(overlay, (10, 10), (250, 45 + 20*7), color=(0, 0, 0), thickness=-1)
     cv.addWeighted(overlay, 0.6, frame_with_text, 1 - 0.6, 0, frame_with_text)
 
     ## Exit
@@ -42,20 +42,44 @@ def Draw_Text(img, is_recording, is_enlarged, is_fliped, is_contrast, is_brightn
     else:
         cv.putText(frame_with_text, '[F]: Flip', Fliped_TextPlace, cv.FONT_HERSHEY_DUPLEX, 0.5, (255, 255, 255))
 
+    ## +/- Controlled:
+    PlusMinus_TextPlace = (20, 30 + 20*4)
+    cv.putText(frame_with_text, '[+/-] Controll:', PlusMinus_TextPlace, cv.FONT_HERSHEY_DUPLEX, 0.5, ( 255, 255, 255))    
+
+    ## Contrast
+    Contrast_TextPlace = (20, 30 + 20*5)
+    if is_contrast:
+        cv.putText(frame_with_text, '  [C]: Contrast ' + str(contrast), Contrast_TextPlace, cv.FONT_HERSHEY_DUPLEX, 0.5, ( 0, 255, 255))
     else:
-        cv.putText(frame_with_text, 'NOT Recording', Recording_TextPlace, cv.FONT_HERSHEY_DUPLEX, 0.5, (255, 255, 0))
+        cv.putText(frame_with_text, '  [C]: Contrast ' + str(contrast), Contrast_TextPlace, cv.FONT_HERSHEY_DUPLEX, 0.5, (255, 255, 255))
+
+    ## Brightness
+    Brightness_TextPlace = (20, 30 + 20*6)
+    if is_brightness:
+        cv.putText(frame_with_text, '  [B]: Brightness ' + str(brightness), Brightness_TextPlace, cv.FONT_HERSHEY_DUPLEX, 0.5, ( 0, 255, 255))
+    else:
+        cv.putText(frame_with_text, '  [B]: Brightness ' + str(brightness), Brightness_TextPlace, cv.FONT_HERSHEY_DUPLEX, 0.5, (255, 255, 255))
+        
+    ## Reset
+    Reset_TextPlace = (20, 30 + 20*7)
+    cv.putText(frame_with_text, '  [R]: Reset', Reset_TextPlace, cv.FONT_HERSHEY_DUPLEX, 0.5, ( 255, 255, 255))
         
     return frame_with_text
 
-def Draw_and_Show(img, is_recording, is_enlarged, is_fliped, is_contrast, is_brightness, is_rotate, contrast, brightness, rotate, mouse_xy):
+def Draw_and_Show(img, is_recording, is_enlarged, is_fliped, is_contrast, is_brightness, contrast, brightness, mouse_xy):
+
     targetImage = img.copy()
+    targetImage = ( 1 + 0.1*contrast)*targetImage + brightness
+    targetImage[targetImage < 0] = 0
+    targetImage[targetImage > 255] = 255
+    targetImage = targetImage.astype(np.uint8)
 
     if is_fliped:
         targetImage = cv.flip(targetImage, 1)
     if is_enlarged:
         targetImage = Enlarge(targetImage, mouse_xy)
 
-    targetImage = Draw_Text(targetImage, is_recording, is_enlarged, is_fliped, is_contrast, is_brightness, is_rotate, contrast, brightness, rotate)
+    targetImage = Draw_Text(targetImage, is_recording, is_enlarged, is_fliped, is_contrast, is_brightness, contrast, brightness)
 
     cv.imshow('Preview', targetImage)   # Play video
 
@@ -104,25 +128,14 @@ def Enlarge(img, mouse_xy, zoom_box_radius = 10, zoom_level = 5, zoom_box_margin
 
     return img_copy
 
-# To Do
-# [] Image Subtraction: S
-# [] Filters: +/-
-    # [] Contrast: C
-    # [] Brightness: B
-    # [] Rotate: R
-# [] Reset: T
-
 is_recording = False
 is_enlarged = False
 is_fliped = False
 is_contrast = False
 is_brightness = False
-is_rotate = False
-is_subtract = False
 
 contrast = 0
 brightness = 0
-rotate = 0
 
 target_fps = 30
 target_fourcc = 'MJPG'
@@ -144,7 +157,7 @@ while True:
         print("Not Valid")
         break
 
-    Draw_and_Show(img, is_recording, is_enlarged, is_fliped, is_contrast, is_brightness, is_rotate, contrast, brightness, rotate, mouse_xy)    # Draw What is needed and show
+    Draw_and_Show(img, is_recording, is_enlarged, is_fliped, is_contrast, is_brightness, contrast, brightness, mouse_xy)    # Draw What is needed and show
     Manage_Recording(is_recording, img, target,  target_fps, target_fourcc) # Record
     key = cv.waitKey(1)
 
@@ -156,6 +169,26 @@ while True:
         is_enlarged = not is_enlarged
     elif key == ord('F') or key == ord('f'):
         is_fliped = not is_fliped
-    
+    elif key == ord('C') or key == ord('c'):
+        is_contrast = True
+        is_brightness = False
+    elif key == ord('B') or key == ord('b'):
+        is_contrast = False
+        is_brightness = True
+    elif key == ord('R') or key == ord('r'):
+        is_contrast = False
+        is_brightness = False
+        contrast = 0
+        brightness = 0
+    elif key == ord('+') or key == ord('='):
+        if is_contrast:
+            contrast += 1
+        if is_brightness:
+            brightness += 1
+    elif key == ord('-') or key == ord('_'):
+        if is_contrast:
+            contrast -= 1
+        if is_brightness:
+            brightness -= 1    
 
 End_Program(video, target)
